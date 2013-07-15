2e913f86a091f71e512a7f435902230e70ddf824
Kimberly Guthrie <kimberlynguthrie@gmail.com>
2013-07-15 18:54:36 -0500

    changed url

--- MovieMobile/src/com/moviemobile/utils/Constant.java
+++ MovieMobile/src/com/moviemobile/utils/Constant.java
@@ -9,7 +9,7 @@ public class Constant {
 	public static MovieBean movieObject = new MovieBean();
 	public static JSONArray movieJsonArr = null;
 	public static int selectedMovieId = -1;
-	public static final String MOVIE_LIST_URL = "http://smartdeventerprise.com/moviemobile/get_movies.php"; 
+	public static final String MOVIE_LIST_URL = "http://smartdeventerprise.com/moviemobile/get_movies_str.php"; 
 	
 	
 
