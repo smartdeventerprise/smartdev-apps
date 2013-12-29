56449348da14bb05759f427e8b8bb2ce4599bbc9
Kimberly Guthrie <kimberlynguthrie@gmail.com>
2013-12-29 07:55:42 -0500

    duplicated

---
+++ MovieMobileV2/src/com/moviemobile/NowShowing.java
@@ -0,0 +1,219 @@
+package com.moviemobile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+
+import com.moviemobile.R;
+import com.moviemobile.utils.Adapter;
+import com.moviemobile.utils.Constant;
+import com.moviemobile.utils.MovieBean;
+import com.moviemobile.utils.Utils;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class NowShowing extends Fragment {
+	
+
+    ListView list;
+    Adapter adapter;
+    public static TextView txtError, txtCinema;
+    Context context;
+	
+	
+	ArrayList<MovieBean> arr = null;
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_now_showing,
+				container, false);
+		
+		context = getActivity();
+		
+		txtCinema = (TextView) rootView.findViewById(R.id.txtCinema);
+		txtError = (TextView) rootView.findViewById(R.id.txtError);
+		Utils.showProgressDialog(context);
+		
+		list=(ListView) rootView.findViewById(R.id.listNowShowing);
+		
+		if (Utils.isNetworkAvailable(context))
+		{		
+			new getMovies().execute();
+		}
+
+		else
+		{
+			Utils.closeProgressDialog(context);
+			
+			txtError.setVisibility(View.VISIBLE);
+			txtError.setText("No Internet Connection");
+			
+		}
+		
+		return rootView;
+	}
+	
+	
+	
+	@Override
+    public void onDestroy()
+    {
+        list.setAdapter(null);
+        super.onDestroy();
+    }
+    
+//    public OnClickListener listener=new OnClickListener(){
+//        @Override
+//        public void onClick(View arg0) {
+//            adapter.imageLoader.clearCache();
+//            adapter.notifyDataSetChanged();
+//        }
+//    };
+	
+	public class getMovies extends AsyncTask<Void, Void,Integer>{
+		public getMovies()
+		{
+			
+		}
+		
+		
+		//private ArrayList<MovieBean> list = new ArrayList<MovieBean>();
+		
+
+		
+		@Override
+		protected void onPostExecute(Integer result)
+		{
+			 if (Constant.movieJsonArr != null)
+			 {
+				 
+				 txtCinema.setVisibility(View.VISIBLE);
+				 
+				 txtCinema.setText("Now Showing");
+				 adapter=new Adapter(getActivity());
+				 
+			     list.setAdapter(adapter);
+		     
+		    
+			 }
+			 else
+			 {
+				 txtCinema.setVisibility(View.GONE);
+				 txtError.setVisibility(View.VISIBLE);
+				txtError.setText("No Movies Found");
+			 } 
+			 Utils.closeProgressDialog(context);
+		}
+		
+		@Override
+		protected Integer doInBackground(Void... params)
+		{
+			
+		try
+		{
+			
+			String movieJSONString = getResponseString(Constant.MOVIE_LIST_URL);
+
+			
+			Constant.movieJsonArr = new JSONArray(movieJSONString);
+			
+
+			
+		}
+		catch(Exception e)
+		{
+			Utils.closeProgressDialog(context);
+		}
+		
+			
+			return null;
+		}
+		
+		
+		
+		public String getResponseString (String url) 
+		{
+			String responseString = "";
+			try {
+				// Setup the object that makes the http request
+				
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpRequestBase request = new HttpGet(url);
+//				request.addHeader(Utils.getBasicAuthenticationHeader(context));
+//				request.addHeader("User-Agent", Constant.HTTP_USER_AGENT);
+				HttpResponse response = httpclient.execute(request);
+				StatusLine statusLine = response.getStatusLine();
+				
+				// If there was no error in the http request
+				if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					response.getEntity().writeTo(out);
+					out.close();
+					responseString = out.toString();
+				} else {
+					response.getEntity().getContent().close();
+					//Log.i("getResponseString", responseString);
+					throw new IOException(statusLine.getReasonPhrase());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				Utils.closeProgressDialog(context);
+				
+				txtCinema.setVisibility(View.GONE);
+				txtError.setVisibility(View.VISIBLE);
+				txtError.setText("No Internet Connection");
+				
+			}
+			return responseString;
+		}
+		
+		
+	}
+
+
+
+	//
+//		  protected Bitmap getImage(String url) {
+//		      
+//		      Bitmap mIcon11 = null;
+//		      try {
+//		        InputStream in = new java.net.URL(url).openStream();
+//		        mIcon11 = BitmapFactory.decodeStream(in);
+//		      } catch (Exception e) {
+//		          System.out.println(e.getMessage());
+//		          e.printStackTrace();
+//		      }
+//		      return mIcon11;
+//		  }
+
+		
+		      
+		
+		
+
+
+	
+    
+    
+}
---
+++ MovieMobileV2/src/com/moviemobile/utils/Adapter.java
@@ -0,0 +1,209 @@
+package com.moviemobile.utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.moviemobile.MovieDetails;
+import com.moviemobile.NowShowing;
+import com.moviemobile.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Adapter extends BaseAdapter {
+    
+    private Activity activity;
+    //private String[] data;
+    private static LayoutInflater inflater=null;
+    public ImageLoader imageLoader; 
+    
+    public Adapter(Activity a) {
+        activity = a;
+        //data=d;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        imageLoader=new ImageLoader(activity.getApplicationContext());
+        int movieCount = 0;
+        try
+        {
+        	Constant.movieFilterJsonArr = new JSONArray();
+        	movieCount = Constant.movieJsonArr.length();
+        	
+        
+        }
+        catch(Exception e)
+        {
+        	NowShowing.txtCinema.setVisibility(View.GONE);
+        	NowShowing.txtError.setText("No Internet Connection");
+        	NowShowing.txtError.setVisibility(View.VISIBLE);
+        	
+        	
+        }
+        
+        
+    
+	        for(int index = 0; index < movieCount; index ++) //filters the movie by cinema
+			{
+	        	JSONObject jObj;
+				try {
+					jObj = Constant.movieJsonArr.getJSONObject(index);
+					
+					 switch(Constant.selectedCinema)
+			            {
+			            	case 0:
+			            		if((jObj.getString("Carib5")).equals("1"))
+			            			Constant.movieFilterJsonArr.put(jObj);
+			    			
+			            		break;
+			            	case 1:
+			            		if((jObj.getString("PalaceCine")).equals("1"))
+			            			Constant.movieFilterJsonArr.put(jObj);
+			    			
+			            		break;
+			            		
+			            	case 2:
+			            		if((jObj.getString("PalaceMulti")).equals("1"))
+			            			Constant.movieFilterJsonArr.put(jObj);
+			    			
+			            		break;
+			            	case 3:
+			            		if((jObj.getString("Odeon")).equals("1"))
+			            			Constant.movieFilterJsonArr.put(jObj);
+			    			
+			            		break;
+			            		
+			            	case 4:
+			            		if((jObj.getString("Marquee")).equals("1"))
+			            			Constant.movieFilterJsonArr.put(jObj);
+			    			
+			            		break;
+			            
+			            }
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					NowShowing.txtCinema.setVisibility(View.GONE);
+					NowShowing.txtError.setText("No Internet Connection");
+					NowShowing.txtError.setVisibility(View.VISIBLE);
+					
+				}
+	           
+        }
+        
+    
+    }
+
+    public int getCount() {
+        //return data.length;
+    	if(Constant.movieFilterJsonArr.length() == 0)
+    	{
+    		NowShowing.txtCinema.setVisibility(View.GONE);
+			NowShowing.txtError.setVisibility(View.VISIBLE);
+			NowShowing.txtError.setText("No Movies Found");
+    	}
+        return Constant.movieFilterJsonArr.length();
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+    
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi=convertView;
+        if(convertView==null)
+            vi = inflater.inflate(R.layout.movie_list_item, null);
+
+        
+        TextView title=(TextView)vi.findViewById(R.id.txtMovieTitle);
+        ImageView image=(ImageView)vi.findViewById(R.id.imagePoster);
+        TextView time = (TextView)vi.findViewById(R.id.txtTime);
+        TextView genre = (TextView)vi.findViewById(R.id.txtGenre);
+        TextView rating = (TextView)vi.findViewById(R.id.txtRating);
+        ImageView image3D=(ImageView)vi.findViewById(R.id.image3d);
+        
+        try{
+        	JSONObject jObj = Constant.movieFilterJsonArr.getJSONObject(position);
+            
+        
+        
+				        vi.setId(jObj.getInt("id"));
+				        
+				        
+				        
+				        imageLoader.DisplayImage((jObj.getString("image")), image);
+				        
+				        title.setText(jObj.getString("movie"));
+				        
+				        switch(Constant.selectedCinema) //filters the movie times based on cinema
+				        {
+				        case 0:
+				        	time.setText(jObj.getString("carib_time"));
+				        	break;
+				        	
+				        case 1:
+				        	time.setText(jObj.getString("palCine_time"));
+				        	break;
+				        	
+				        case 2:
+				        	time.setText(jObj.getString("palMulti_time"));
+				        	break;
+				        	
+				        case 3:
+				        	time.setText(jObj.getString("odeon_time"));
+				        	break;
+				        	
+				        case 4:
+				        	time.setText(jObj.getString("marquee_time"));
+				        	break;
+				        	
+				        }
+				        
+				        genre.setText(jObj.getString("genre"));
+				        rating.setText(jObj.getString("rating"));
+				        
+				        if((jObj.getString("3d").equals("1")))
+				        {
+				        	image3D.setVisibility(View.VISIBLE);
+				        }
+		
+
+        }
+        
+        
+        catch(Exception e)
+        {
+        	System.out.println(e.getMessage());
+        }
+        
+       
+        
+        vi.setOnClickListener(new OnClickListener() {
+    		@Override
+    		public void onClick(View v) {
+    			
+    			Constant.selectedMovieId=v.getId();
+    			Intent in = new Intent(activity,MovieDetails.class);
+    			activity.startActivity(in);
+    			
+    			
+    		}
+        
+        });
+        
+        return vi;
+    }
+    
+    
+}
